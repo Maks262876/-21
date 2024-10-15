5f04ac92f2f4c7e6b2c8347f3329978b59ceb961
Maksim <prom8142@gmail.com>
2024-10-15 10:40:09 +0300

    New

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 console.log("Hello world")
 alert("Привіт із зовнішнього файлу!")
+function calculateSum(a, b) {
+    return a + b;
+}
+function calculateDifference(a, b) {
+    return a - b;
+}
+let num1 = 10;
+let num2 = 5;
+console.log("Сума:", calculateSum(num1, num2));
+console.log("Різниця:", calculateDifference(num1, num2));
