d724a6a19f3b907360dfb38e3d49a56bbbb79c5e
Maksim <prom8142@gmail.com>
2024-10-15 12:58:38 +0300

    New2

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 console.log("Hello world")
 alert("Привіт із зовнішнього файлу!")
 
-let = 10;
-let = 5;
-console.log ("Сума: ")
-console.log ("Різниця: ")
+let a = 10;
+let b = 5;
+
+let suma = a + b;
+let riznytsia = a - b;
+
+console.log("Сума: " + suma);
+console.log("Різниця: " + riznytsia)
