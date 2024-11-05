30a3f4fafd635f99b700e1c606a639ecfbc6d3e9
Maksim <prom8142@gmail.com>
2024-11-05 15:28:31 +0200

    New5

--- jsBasics/task5.js
+++ jsBasics/task5.js
@@ -1,12 +1,12 @@
 let testScore = 78
-if (testScore < 60 ) {
-    console.log ("Незадовільно")
-} else if (testScore >= 60 && testScore <= 70) {
-    console.log ("Задовільно")
-} else if (testScore >= 71 && testScore <= 80) {
-    console.log ("Добре")
-} else if (testScore >= 81 && testScore <= 90) {
-    console.log ("Дуже добре")
-} else if (testScore >= 91 && testScore <= 100) {
-    console.log ("Відмінно")
+if (testScore < 50 ) {
+    console.log ("Потрібно покращити знання")
+} else if (testScore >= 50 && testScore <= 64) {
+    console.log ("Середній рівень")
+} else if (testScore >= 65 && testScore <= 79) {
+    console.log ("Хороший рівень")
+} else if (testScore >= 80 && testScore <= 89) {
+    console.log ("Дуже хороший рівень")
+} else if (testScore >= 90 && testScore <= 100) {
+    console.log ("Відмінний рівень")
 }
