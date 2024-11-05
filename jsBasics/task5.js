ff016ccb7be5778566d7802b494dae141a26d2ae
Maksim <prom8142@gmail.com>
2024-11-05 15:20:31 +0200

    New5

---
+++ jsBasics/task5.js
@@ -0,0 +1,12 @@
+let testScore = 78
+if (testScore < 60 ) {
+    console.log ("Незадовільно")
+} else if (testScore >= 60 && testScore <= 70) {
+    console.log ("Задовільно")
+} else if (testScore >= 71 && testScore <= 80) {
+    console.log ("Добре")
+} else if (testScore >= 81 && testScore <= 90) {
+    console.log ("Дуже добре")
+} else if (testScore >= 91 && testScore <= 100) {
+    console.log ("Відмінно")
+}
