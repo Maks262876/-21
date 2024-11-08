e51f427c1d599ea225087b678ff10635f29b4f0b
Maksim <prom8142@gmail.com>
2024-11-08 14:06:03 +0200

    New6

---
+++ jsBasics/task6.js
@@ -0,0 +1,10 @@
+function calculateArea (width,height) {
+    const result = width * height
+    return result
+}
+const resultArea = calculateArea(5,10)
+console.log (resultArea)
+
+
+const calculateArea = (width, height) => width * height;
+console.log(calculateArea(5, 10))
