47754709f85712937a1edf6523e27f1a295c44a6
Maksim <prom8142@gmail.com>
2024-11-20 10:21:13 +0200

    New8.1

--- jsBasics/task8.js
+++ jsBasics/task8.js
@@ -1,4 +1,7 @@
 const numbers = [10, 20, 30, 40, 50];
+let totalSum = 0;
+
 for (let i = 0; i < numbers.length; i++) {
-    console.log (numbers[i])
+    totalSum += numbers[i];
 }
+console.log("Сума всіх елементів масиву:", totalSum);
