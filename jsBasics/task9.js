397ac9b2f46a13a4e06b57d9acbd95c9d979c91d
Maksim <prom8142@gmail.com>
2024-11-26 10:12:09 +0200

    New9

---
+++ jsBasics/task9.js
@@ -0,0 +1,10 @@
+const person = {
+    firstName: "Максим",
+    lastName: "Сім'яник",
+    age: 17
+  }
+  
+  person.email = "maksymsimianyk@gmail.com"
+  
+  delete person.age
+  console.log(person)
